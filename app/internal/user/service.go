58d602595f286a3f5dff21e7cbd71a0624d54ccd
Andre Parelho <parelhoneves.andre@gmail.com>
2026-01-01 22:48:49 -0300

    feat: update service

--- app/internal/user/service.go
+++ app/internal/user/service.go
@@ -1,21 +1,53 @@
 package user
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/andreparelho/user-api-crud/pkg/dynamo"
+	"github.com/andreparelho/user-api-crud/pkg/repository"
+	"github.com/gofiber/fiber/v3"
+)
 
 type UserService interface {
 	CreateUser() fiber.Handler
 	GetUser() fiber.Handler
 }
 
-func NewUserService() UserService {
-	return &user{}
+func NewUserService(repo repository.UserRepository, logger *slog.Logger) UserService {
+	return &user{
+		Repository: repo,
+		Logger:     logger,
+	}
 }
 
 func (u *user) CreateUser() fiber.Handler {
 	return func(ctx fiber.Ctx) error {
-		return ctx.JSON(fiber.Map{
-			"ok": true,
-		})
+		var user User
+		if err := json.Unmarshal(ctx.Body(), &user); err != nil {
+			u.Logger.ErrorContext(ctx.Context(), "Erro ao fazer o unmarshall do usuario.",
+				slog.Any("json", (ctx.Body())),
+				slog.Any("error", err),
+			)
+			return ctx.SendStatus(http.StatusBadRequest)
+		}
+
+		u.SetIdToUser(&user)
+
+		dynamoUser := dynamo.User{
+			ID:    user.Id,
+			Name:  user.Nome,
+			Email: user.Email,
+		}
+		if err := u.Repository.Save(ctx.Context(), dynamoUser); err != nil {
+			u.Logger.ErrorContext(ctx.Context(), "Erro ao salvar o usuario no dynamo.",
+				slog.Any("error", err),
+			)
+			return ctx.SendStatus(http.StatusInternalServerError)
+		}
+
+		return ctx.SendStatus(http.StatusAccepted)
 	}
 }
 
--- app/internal/user/types.go
+++ app/internal/user/types.go
@@ -1,9 +1,23 @@
 package user
 
+import (
+	"log/slog"
+
+	"github.com/andreparelho/user-api-crud/pkg/repository"
+	"github.com/google/uuid"
+)
+
 type User struct {
 	Id    string `json:"-"`
 	Nome  string `json:"nome"`
 	Email string `json:"email"`
 }
 
-type user struct{}
+type user struct {
+	Repository repository.UserRepository
+	Logger     *slog.Logger
+}
+
+func (u user) SetIdToUser(user *User) {
+	user.Id = uuid.New().String()
+}
